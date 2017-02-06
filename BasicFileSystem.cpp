a207ce0406e8a7d1e3e71dbd3e504804649a4de5
Jobarah <barahona.josue@unitec.edu>
2017-02-06 15:22:50 -0600

    read modified

--- BasicFileSystem.cpp
+++ BasicFileSystem.cpp
@@ -22,13 +22,27 @@ bool BasicFileSystem::writeBlock(string diskName, int blockNumber, Block* block,
 }
 
 Block BasicFileSystem::readBlock(string diskName, int blockNumber, int blockSize) {
-    cout<< diskName << endl;
-    ifstream in(diskName, ios::in|ios::binary|ios::ate);
-    Block *returnBlock;
-    char *readBlock;
+
+    ifstream in;
+    Block *returnBlock = new Block();
+    in.open(diskName, ios::in|ios::binary);
     in.seekg(blockNumber * blockSize);
-    in.read(readBlock, blockSize);
-    returnBlock = (Block*)readBlock;
+    in.read((char*)&returnBlock, blockSize);
     in.close();
     return *returnBlock;
 }
+
+//in.open(path,ios::in|ios::binary);
+//in.seekg(0);
+//SuperBlock * superblock = new SuperBlock();
+//in.read((char*)&superblock,sizeof(SuperBlock));
+//in.close();
+//return superblock->tamanoDeBloques;
+
+//in.open(diskName, ios::out|ios::binary|ios::app);
+//char * memBlock = (char*)calloc(1, blockSize);
+//memcpy(memBlock, block, blockSize);
+//out.seekp(blockNumber * blockSize);
+//out.write(memBlock, blockSize);
+//delete [] memBlock;
+//out.close();
